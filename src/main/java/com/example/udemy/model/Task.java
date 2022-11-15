a9caf7a9e38542eed23147e625fd804235d4af24
Cyprian <dranags@gmail.com>
2022-11-15 13:46:49 +0100

    Initial commit

---
+++ src/main/java/com/example/udemy/model/Task.java
@@ -0,0 +1,41 @@
+package com.example.udemy.model;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank(message = "Task description can't be null !")
+    private String description;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
