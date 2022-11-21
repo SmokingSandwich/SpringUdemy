6f552d890646d4b9c8df36f242df191c1da5cde4
Cyprian <cyprian.owczarczak@gmail.com>
2022-11-21 12:23:44 +0100

    Lesson 82 task check

--- src/main/java/com/example/udemy/adapter/SqlTaskGroupRepository.java
+++ src/main/java/com/example/udemy/adapter/SqlTaskGroupRepository.java
@@ -3,8 +3,15 @@ package com.example.udemy.adapter;
 import com.example.udemy.model.TaskGroup;
 import com.example.udemy.model.TaskGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+    @Override
+    @Query("select distinct g from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
+
 }
