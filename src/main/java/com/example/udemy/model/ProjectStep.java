8655e98a96d00df76ec9b735262be3277703f252
Cyprian <dranags@gmail.com>
2022-11-22 11:18:38 +0100

    Lesson 85

---
+++ src/main/java/com/example/udemy/model/ProjectStep.java
@@ -0,0 +1,50 @@
+package com.example.udemy.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "project_Steps")
+public class ProjectStep {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Project step's description can't be null !")
+    private String description;
+    private int daysToDeadline;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDaysToDeadline() {
+        return daysToDeadline;
+    }
+
+    void setDaysToDeadline(int daysToDeadline) {
+        this.daysToDeadline = daysToDeadline;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
