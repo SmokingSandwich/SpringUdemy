a50222ab76557c87624962877cd17d85d91ac0e9
Cyprian <dranags@gmail.com>
2022-11-25 16:31:27 +0100

    Lessons 121

--- src/main/java/com/example/udemy/adapter/SqlTaskRepository.java
+++ src/main/java/com/example/udemy/adapter/SqlTaskRepository.java
@@ -5,10 +5,7 @@ import com.example.udemy.model.TaskRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
